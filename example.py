b83e82d7ee24ed35012afe7705fc87d2aa25d676
Rado <R.A.Todorov@student.tudelft.nl>
2022-08-08 10:07:36 +0200

    Look at new commit of submodule

--- example.py
+++ example.py
@@ -3,7 +3,7 @@ from .Nested.consumer import Consumer
 if __name__ == "__main__":
     cons = Consumer()
     print(cons.get_count())
-    cons.increment()
+    cons.increment(5)
     print(cons.get_count())
     cons.decrement()
     print(cons.get_count())
